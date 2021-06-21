43aae4fa0bce75c1653535226ead5910c7d2640a
swillrich5 <swillrich@gmail.com>
2021-06-20 20:04:18 -0500

    individual character search working, needs layout

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Character = ({match}) => {
     console.log(match.params.uid);
+
+    const [loading, setLoading] = useState(false);
+    const [character, setCharacter] = useState({});
+
+    useEffect(() => {
+        getCharacterDetail(match.params.uid);
+    }, []);
+
+    const getCharacterDetail = async (id) => {
+        setLoading(true);
+        const res = await axios.get("http://stapi.co/api/v1/rest/character?uid=" + id)
+        console.log(res.data.character);
+        setLoading(false);
+        setCharacter(res.data.character);
+    }
+
     return (
 
 
         <div className="container space-background">
             <div className="jumbotron">
+                <h4>Character Name: {character.name}</h4>
                 <div>
-                    {match.params.uid}
+                    <table>
+                        
+                    </table>
                 </div>
         </div>
     </div>
